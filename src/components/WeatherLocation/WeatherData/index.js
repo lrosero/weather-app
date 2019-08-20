3cb273c4b32d6d41ea9a31ba551a9c2f12387f8a
lrosero <lumariarv@gmail.com>
2019-08-20 08:17:30 -0500

    Doble destructuring en los index de WeatherData y WeatherLocation.

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import{
-	SUN,
-} from './../../../constants/weathers';
 import './styles.css';
 
-const WeatherData = () => (
+const WeatherData = ({data: {temperature, weatherState, humidity, wind}}) => (
+	
  <div className="weatherDataCont" >
 	 <WeatherTemperature 
-	 	temperature={20} 
-	 	weatherState={SUN}/>
-     <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
+	 	temperature={temperature} 
+	 	weatherState={weatherState}/>
+     <WeatherExtraInfo humidity={humidity} wind={wind}/>
  </div>
 );
+
+WeatherData.propTypes ={
+	data: PropTypes.shape({
+		temperature: PropTypes.number.isRequired,
+		weatherState: PropTypes.string.isRequired,
+		humidity: PropTypes.number.isRequired,
+		wind: PropTypes.string.isRequired,
+	})
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import{
+	SUN,
+} from './../../constants/weathers';
+
+const data ={
+	temperature: 5,
+	weatherState: SUN,
+	humidity:10,
+	wind: '10m/s',
+}
+
 
 const WeatherLocation = () => (
     <div className="weatherLocationCont"> 
         <Location city = {"Colombia"} />
-        <WeatherData/>
+        <WeatherData data={data}/>
     </div>
 );
 export default WeatherLocation;
